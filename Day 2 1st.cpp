d536e21952628b1c38eb894996ac334e1ed7b1f8
sasmitha-sudusinghe <sasmithasudusinghe@gmail.com>
2024-10-19 00:59:22 +0530

    Country and City
    Asking country and city personally

---
+++ Day 2 1st.cpp
@@ -0,0 +1,15 @@
+
+# include <iostream>
+using namespace std;
+int main ()
+{
+    string city;
+    string country;
+    cout<< "Enter your City: ";
+    cin>> city;
+    cout<< "Enter your Country: ";
+    cin>> country;
+    cout<<"You live in "<<city<<", "<<country;
+    return 0;
+
+}
