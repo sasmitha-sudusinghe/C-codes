224859cc6a4b56b0542b99c9b765aaca6b8581a0
sasmitha-sudusinghe <sasmithasudusinghe@gmail.com>
2024-10-18 22:37:33 +0530

    Printing name and age
    Printing name and age with a personalized greeting.

---
+++ Day 1 4th.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+    string name;
+    int age;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<"Welcome "<<name<<" Your age is "<<age;
+
+    return 0;
+}
